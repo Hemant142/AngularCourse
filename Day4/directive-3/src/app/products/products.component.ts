9c884ebc99eb4fad81af7a2c25fc69f5e1db08a5
Hemant142 <yhemant203@gmail.com>
2024-03-19 18:38:14 +0530

    Directive 3

---
+++ Day4/directive-3/src/app/products/products.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+export class Product {
+  name: string;
+  price: number;
+  available: boolean;
+
+  constructor(name: string, price: number, available: boolean) {
+    this.name = name;
+    this.price = price;
+    this.available = available;
+  }
+}
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products: Product[] = [
+    new Product('Laptop', 1200, true),
+    new Product('Smartphone', 800, true),
+    new Product('Headphones', 100, false),
+    new Product('Camera', 600, true),
+    new Product('Tablet', 300, false),
+    new Product('Fitness Tracker', 50, true),
+    new Product('Bluetooth Speaker', 80, true),
+    new Product('Gaming Console', 400, false),
+    new Product('Smartwatch', 150, true),
+    new Product('External Hard Drive', 120, true)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
