1a8caa0dfef257ffeeda30d007e81a6853acd316
Hemant142 <yhemant203@gmail.com>
2024-03-23 23:09:12 +0530

    Finished

--- Evaluation/problem-2/src/app/app-routing.module.ts
+++ Evaluation/problem-2/src/app/app-routing.module.ts
@@ -1,5 +1,3 @@
-// src/app/app-routing.module.ts
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
