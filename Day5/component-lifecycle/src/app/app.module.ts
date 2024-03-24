d22c88d24224a613d5f0d3b5e7796f0cf50e11d9
Hemant142 <yhemant203@gmail.com>
2024-03-24 22:19:29 +0530

    Managing Component Lifecycle with Angular Hooks

---
+++ Day5/component-lifecycle/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
+import { AppComponent } from './app.component';
+import { LifecycleDemoComponent } from './lifecycle-demo/lifecycle-demo.component';
+import { DataService } from './data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LifecycleDemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule 
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
